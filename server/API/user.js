a2de8cad49ec52d87b08b88f408378d283de3098
Strange4 <>
2024-09-25 18:07:23 -0400

    add aditional parameters for user creation

--- server/API/user.js
+++ server/API/user.js
@@ -12,9 +12,17 @@ router.use(express.json(jsonConfigs));
 // creating a user in the database
 router.route("/create")
     .post(async (req, res) => {
-        const username = req.body.username;
-        const password = req.body.password;
-        const goodResult = await createUser(db, username, password);
+        const userObject = {
+            username: req.body.username,
+            password_hash: req.body.password_hash,
+            salt: req.body.salt,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            schoolID: req.body.schoolID,
+            role: req.body.role
+        }
+        const goodResult = await createUser(db, userObject);
         if (goodResult) {
             res.json({ created: true });
         } else {
--- server/database/API.js
+++ server/database/API.js
@@ -1,13 +1,23 @@
 import pg from 'pg'
+import log from '../logger.js'
 /**
  * 
- * @param {string} username the username of the new user
- * @param {string} password_hash the password of the new user (must be a hexadecimal hash)
+ * @param {object} userObject the object containing all the information about a user
+ * @param {string} userObject.username
+ * @param {string} userObject.password_hash
+ * @param {string} userObject.salt
+ * @param {string} userObject.firstName
+ * @param {string} userObject.lastName
+ * @param {string} userObject.email
+ * @param {string} userObject.schoolID
+ * @param {string} userObject.role
  * @returns {bool} true if the username and password are valid, false otherwise
  */
-function verifyUser(username, password_hash) {
-    if (username.length == 0 || password_hash.length == 0) {
-        return false
+function verifyUser(userObject) {
+    for (const key of Object.keys(userObject)) {
+        if (!key) {
+            return false;
+        }
     }
     // TODO: verification for the username and passwords (verify lengt, hash, etc.)
     return true
@@ -16,27 +26,39 @@ function verifyUser(username, password_hash) {
 /**
  * 
  * @param {pg.Client} db the database to query
- * @param {string} username the username of the new user to create
- * @param {string} password_hash the password of the new user (must be a hexadecimal hash)
+ * @param userObject the object containing all the information about a user
  * @returns {bool} true if the user could be created, false otherwise
  */
-async function createUser(db, username, password_hash) {
-    if (!verifyUser(username, password_hash)) {
-        console.log("user can't be created")
+async function createUser(db, userObject) {
+    if (!verifyUser(userObject)) {
+        log.warn("user can't be created")
         return false
     }
     const table = "users"
+    let hash = undefined;
+    try {
+        hash = Buffer.from(userObject.password_hash, "hex")
+    } catch (error) {
+        return false;
+    }
     const query = {
         name: "register-user",
-        text: `INSERT INTO ${table} (username, password) VALUES ($1, $2)`, // this query is wrong
-        values: [username, password_hash]
+        text: `INSERT INTO ${table} (username, hash, salt, f_name, l_name, email, school_id, role) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+        values: [
+            userObject.username, hash,
+            userObject.salt, userObject.firstName,
+            userObject.lastName, userObject.email,
+            userObject.schoolID, userObject.role
+        ]
     }
 
     try {
         await db.query(query);
     } catch (error) {
-        console.error("There was an error while querying the database");
-        console.error(error);
+        log.error("There was an error while querying the database");
+        log.error(error);
+        return false;
     }
 
     return true
