bfc546511c2f6a209af11befcdac01d02f8f9ca2
Strange4 <83730323+Strange4@users.noreply.github.com>
2024-09-26 18:31:44 -0400

    add build and populate scripts for teams

--- server/database/scripts/drop_db.js
+++ server/database/scripts/drop_db.js
@@ -1,12 +1,16 @@
+import fs from 'node:fs'
 import { db } from "../db.js";
 
+// Drop teams
+await db.query(fs.readFileSync(import.meta.dirname + "/sql/teams_drop.sql").toString());
+console.log("Dropped teams");
+
 //Drop users table
-await db.query("DROP TABLE IF EXISTS users");
+await db.query(fs.readFileSync(import.meta.dirname + "/sql/users_drop.sql").toString());
 
 console.log("Dropped users table");
 
 //TODO: Drop Courses table
 
-//TODO: Drop Teams table
 
 db.end();
--- server/database/scripts/populate_db.js
+++ server/database/scripts/populate_db.js
@@ -5,6 +5,8 @@ let usernum = 0;
 let schoolNum = 10000000;
 let getUserNum = () => ++usernum;
 let getSchoolNum = () => ++schoolNum;
+const getRandomNumber = (min, max) => (Math.random() * (max - min)) + min;
+const getRandomInt = (min, max) => Math.floor(getRandomNumber(min, max));
 
 //Make students
 for (let i = 0; i < 11; i++) {
@@ -40,5 +42,19 @@ console.log("Populated users table");
 //TODO: Populate Courses table
 
 //TODO: Populate Teams table
+
+const numberOfTeams = 10;
+for (let team_id = 1; team_id <= numberOfTeams; team_id++) {
+  await db.query("INSERT INTO teams (team_name) VALUES ($1)",
+    [`test_team_${team_id}`]
+  );
+  for (let index = 0; index < getRandomInt(1, 5); index++) {
+    await db.query("INSERT INTO team_members (user_id, team_id) VALUES ($1, $2)",
+      [getRandomInt(1, 11), team_id]
+    )
+  }
+}
+
+console.log("populated the teams");
 
 db.end();
