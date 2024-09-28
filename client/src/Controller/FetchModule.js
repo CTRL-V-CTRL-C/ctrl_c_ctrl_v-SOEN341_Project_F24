e4849f6313f7acfd64dacc5913cd0076a52e42e0
RimDallali <dallali.rim1@gmail.com>
2024-09-27 23:39:13 -0400

    add a fetch module to avoid redundancy when fetching and maintaining clean code

---
+++ client/src/Controller/FetchModule.js
@@ -0,0 +1,50 @@
+
+/**
+ * Generic fetch function to fetch from any given
+ * url and return a json object with the contained data
+ * 
+ * @param {String} url 
+ * @returns {JSON} data
+ */
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            return await response.json();
+        } else {
+            throw Error("Something Went wrong fetching data");
+        }
+    } catch {
+        return undefined;
+    }
+}
+
+/**
+ * Generic post function to post/put given data using
+ * the given method (post/put)
+ * @param {String} url 
+ * @param {Object} body 
+ * @param {String} method 
+ * @returns {Object}
+ */
+async function postData(url, body, method) {
+    try {
+        const response = await fetch(url, {
+            method: method,
+            body: JSON.stringify(body),
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json"
+            },
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            throw Error("Something Went wrong posting data");
+        }
+    } catch {
+        return undefined;
+    }
+}
+
+export { fetchData, postData };
