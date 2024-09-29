b5dd8ba333cbbfac22672dfac1d6f8fb5d2274ee
Nolan Ganz <nolanganz1@gmail.com>
2024-09-28 20:39:42 -0400

    Modified account creation to perform hashing on the backend
    Removed hex encoding from insert query since it was breaking it

--- server/API/user.js
+++ server/API/user.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import { createUser } from '../database/API.js'
-import { db } from '../database/db.js'
+import express from 'express';
+import { createUser } from '../database/API.js';
+import { db } from '../database/db.js';
+import argon2 from 'argon2';
 
 const router = express.Router();
 
@@ -14,14 +15,14 @@ router.route("/create")
     .post(async (req, res) => {
         const userObject = {
             username: req.body.username,
-            password_hash: req.body.password_hash,
-            salt: req.body.salt,
+            password: req.body.password,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             email: req.body.email,
             schoolID: req.body.schoolID,
             role: req.body.role
         }
+        userObject.password_hash = await argon2.hash(userObject.password);
         const goodResult = await createUser(db, userObject);
         if (goodResult) {
             res.json({ created: true });
