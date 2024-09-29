8ec9b8fe49d0739b975fbca0fca787b09cea8ea6
Nolan Ganz <nolanganz1@gmail.com>
2024-09-29 01:44:46 -0400

    Connected login to backend

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -7,47 +7,74 @@ function LoginPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
 
-    function loginAccount(event) {
+    async function loginAccount(event) {
 
         //Stops the form from submitting
         event.preventDefault();
 
-        // Get stored user data from localStorage
-        const storedUserData = localStorage.getItem("userData");
+        //Prevent multiple submissions
+        setIsButtonDisabled(true);
 
-        if (storedUserData) {
-            // Parse the stored user data
-            const parsedUserData = JSON.parse(storedUserData);
+        //Login post
+        const loginURL = new URL("/api/login", location.origin);
+        const loginResponse = await fetch(loginURL, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: email, password: password })
+        });
 
-            // Compare the email and password entered with the stored data
-            if (
-                email === parsedUserData.email &&
-                password === parsedUserData.password
-            ) {
-                console.log("Login successful!"); //test
-                setMessage("Login successful!");//just for now (should redirect)
-            } else {
-                console.log("Invalid email or password."); //test
-                setMessage("Invalid email or password.");
-            }
+        const loginJSON = await loginResponse.json();
+
+        if (loginResponse.status === 200) {
+            setMessage(loginJSON.msg);
         } else {
-            console.log("Invalid email or password.");//test
-            setMessage("Invalid email or password.");
+            setMessage(loginJSON.msg);
         }
+
+
+
+        // // Get stored user data from localStorage
+        // const storedUserData = localStorage.getItem("userData");
+
+        // if (storedUserData) {
+        //     // Parse the stored user data
+        //     const parsedUserData = JSON.parse(storedUserData);
+
+        //     // Compare the email and password entered with the stored data
+        //     if (
+        //         email === parsedUserData.email &&
+        //         password === parsedUserData.password
+        //     ) {
+        //         console.log("Login successful!"); //test
+        //         setMessage("Login successful!");//just for now (should redirect)
+        //     } else {
+        //         console.log("Invalid email or password."); //test
+        //         setMessage("Invalid email or password.");
+        //     }
+        // } else {
+        //     console.log("Invalid email or password.");//test
+        //     setMessage("Invalid email or password.");
+        // }
+
+        setIsButtonDisabled(false);
     };
 
     return (
         <div className="registration-form">
             <form className="form login-form" onSubmit={(e) => loginAccount(e)}>
                 <p className="title">Login </p>
-                <FormInput fieldName={"Email"} fieldType={"email"} setField={setEmail} />
+                <FormInput fieldName={"Email"} fieldType={"text"} setField={setEmail} />
                 <FormInput fieldName={"Password"} fieldType={"password"} setField={setPassword} />
                 <div className="error-message">
                     {message && <p>{message}</p>}
                 </div>
-                <button disabled={false} className="submit" type='submit'>Sign In</button>
+                <button disabled={isButtonDisabled} className="submit" type='submit'>Sign In</button>
                 <p className="signin">Don&apos;t have an account? <a href="/registerAccount">Create Account</a>  </p>
             </form>
         </div>
