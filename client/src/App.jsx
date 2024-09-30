42878ae69aeb4966999e981b0d0a796d77eca590
RimDallali <dallali.rim1@gmail.com>
2024-09-29 22:15:02 -0400

    add an authentication context for login/logout, not fully functional yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,38 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import PageHolder from './Pages/PageHolder';
 import NavBar from './Pages/Components/NavBar';
 import './Pages/Components/Styles/BackgroundImage.css';
+import AuthContext from './Context/AuthContext';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     if (!userLoggedIn) {
+  //       const authResponse = await fetch("/test-authentication");
+  //       if (authResponse.status === 200) {
+  //         setUserLoggedIn(true);
+  //         console.log("1")
+  //       } else {
+  //         setUserLoggedIn(false)
+  //       }
+  //     }
+  //   })();
+  //   console.log("looged in: " + userLoggedIn)
+  // }, [userLoggedIn]);
 
   return (
-    <>
+    <AuthContext.Provider value={{
+      userLoggedIn,
+      setUserLoggedIn
+    }}>
+
       <NavBar />
       <Router>
         <PageHolder />
       </Router>
-    </>
+    </AuthContext.Provider>
   )
 }
 
--- client/src/Pages/Components/NavBar.jsx
+++ client/src/Pages/Components/NavBar.jsx
@@ -1,24 +1,48 @@
 import './Styles/NavBar.css';
+import AuthContext from '../../Context/AuthContext';
+import { useContext } from "react";
 
+/**
+ * Navigation bar to be used on all pages
+ * @returns {ReactElement}
+ */
 function NavBar() {
+    const auth = useContext(AuthContext);
 
-    //const [userLoggedIn, setUserLoggerIn] = useState(false);
+    async function handleLogout() {
+        await fetch("/api/logout");
+        auth.setUserLoggedIn(false);
+        console.log("LOGGING OUT")
+    }
 
     return (
         <div id="navbar">
-            <div className="nav-sub">
+            <div id="nav-sub">
                 <li>
-                    <a href="/"> Home </a>
+                    <a href="/">
+                        Home
+                    </a>
                 </li>
             </div>
             <div className="nav-sub">
-                <li>
-                    <a href="/loginAccount"> Login </a>
-                </li>
-                <li>
-                    <a href="/registerAccount"> Sign Up </a>
-                </li>
-            </div> 
+                {
+                    auth.userLoggedIn ?
+                        <a href='/'>
+                            <p onClick={async () => { await handleLogout() }} href="/home">
+                                Logout
+                            </p>
+                        </a>
+                        :
+                        <>
+                            <li>
+                                <a href="/loginAccount"> Login </a>
+                            </li>
+                            <li>
+                                <a href="/registerAccount"> Sign Up </a>
+                            </li>
+                        </>
+                }
+            </div>
         </div>
     );
 }
