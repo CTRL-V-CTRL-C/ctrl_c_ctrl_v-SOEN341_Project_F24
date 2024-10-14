af325f25e5fbd1b0e19c09f4b99643bd07cb804f
Strange4 <83730323+Strange4@users.noreply.github.com>
2024-10-14 12:53:10 -0400

    add some test functions

--- server/test/teams.js
+++ server/test/teams.js
@@ -4,6 +4,11 @@ import request from 'supertest'
 import { app } from '../server.js'
 import { db, pool } from '../database/db.js'
 
+const UserRole = {
+    Student: "STUD",
+    Instructor: "INST"
+}
+
 function randomLetters(maxLength) {
     const r = (Math.random() + 1).toString(36).substring(2)
     if (maxLength) {
@@ -12,8 +17,9 @@ function randomLetters(maxLength) {
     return r
 }
 
-async function createUser() {
+async function createUser(userRole) {
     const email = `test.${randomLetters()}@mail.com`;
+    const password = "1234";
     const user = {
         username: email,
         password: "some password",
@@ -21,15 +27,32 @@ async function createUser() {
         lastName: "test_user",
         email,
         schoolID: `${randomLetters(8)}`,
-        role: "STUD"
+        role: userRole
     }
-    const response = await request(app)
+    await request(app)
         .post("/api/user/create")
         .set("Accept", "application/json")
         .send(user)
         .timeout(1000)
-        .expect(200); // timesout after 1 second in case the app crashes
-    return email;
+        .expect(200);
+    return { email, password };
+}
+
+async function loginUser(email, password) {
+    // TODO: login the user
+}
+
+async function createCourse(instructorEmail, instructorPassword) {
+    await loginUser(instructorEmail, instructorPassword);
+    const course = {
+        courseName: `test_course_${randomLetters()}`,
+    };
+    const response = await request(app)
+        .post("/api/courses/create")
+        .set("Accept", "application/json")
+        .send(course)
+        .timeout(1000)
+        .expect(200);
 }
 
 const testEmails = [];
@@ -51,6 +74,7 @@ suite("POST requests to create a team", () => {
     it("should respond with 200 when creating a team with no members", async (t) => {
         const team = {
             teamName: "test_team",
+            courseId: ""
             members: [],
         }
         const response = await request(app)
