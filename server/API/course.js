7648fc69be89f2928b4e3b1dc1f009670789ae4b
Nolan Ganz <nolanganz1@gmail.com>
2024-10-14 17:52:21 -0400

    Added endpoint to get students in a course as an instructor

--- server/API/course.js
+++ server/API/course.js
@@ -28,4 +28,21 @@ router.get("/get-courses", requireAuth, async (req, res) => {
   }
 });
 
+router.get("/get-students/:courseId", requireAuth, async (req, res) => {
+  const courseId = req.params.courseId;
+  try {
+    let hasCourse = await db.query("SELECT course_id FROM courses WHERE instructor_id = $1 AND course_id = $2", [req.session.user.userId, courseId]);
+    if (!req.session.user.isInstructor || hasCourse.rows.length != 1) {
+      res.status(401).json({ msg: "You do not have permission to get the students for this course" });
+    } else {
+      let studentQuery = await db.query("SELECT DISTINCT f_name, l_name, school_id, email, u.user_id FROM courses c JOIN teams t ON c.course_id = t.course_id JOIN team_members tm ON t.team_id = tm.team_id JOIN users u ON tm.user_id = u.user_id WHERE c.course_id = $1", [courseId]);
+      res.status(200).json(studentQuery.rows);
+    }
+
+  } catch (error) {
+    log.error(error, `Something went wrong trying to get all the students in a course for ${req.session.user.email}`);
+    res.status(500).json({ msg: "Something went wrong trying to get your students, please try again later" });
+  }
+});
+
 export { router };
