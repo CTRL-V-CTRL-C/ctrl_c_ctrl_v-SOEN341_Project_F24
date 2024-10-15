48a983c840cee7e2446832582d08cef60318572a
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-10-15 18:36:21 -0400

    added the student info table

--- client/src/Pages/Components/Teams/MembersPage.jsx
+++ client/src/Pages/Components/Teams/MembersPage.jsx
@@ -2,10 +2,29 @@ import '../Styles/MembersPage.css';
 
 function MembersPage() {
     return (
-      <div id="TitlePosition"><p id="PageTitle">
-            Student List
-        </p>
-        </div>  
+<div id="whole">
+            <div id="TitlePosition"><p id="PageTitle">Student List</p></div>  
+    <div>
+        <table id="TableStyle"> 
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Id</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td> FirstName LastName</td>
+                    <td>STUDXXXXX</td>
+                    <td>studentemail@gmail.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+        
     )
 }
 
