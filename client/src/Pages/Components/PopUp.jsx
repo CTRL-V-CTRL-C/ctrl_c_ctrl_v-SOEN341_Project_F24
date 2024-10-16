5d00178aca7a58e0a9a6cdf449b66a816cbf791e
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-10-16 19:52:41 -0400

    added some more details for the popup, added more backend to close popup

--- client/src/Pages/Components/PopUp.jsx
+++ client/src/Pages/Components/PopUp.jsx
@@ -13,32 +13,36 @@ function PopUp(props) {
         }
     };
 
+    const handleClose = (e) => {
+        setFile(null); // Reset the file state
+        props.setTrigger(false); // Close the popup
+    };
+
     // const handleUpload = async () => {
     //     // We will fill this out later
     //   };
 
-    //pass in true for false value to trigger the pop up 
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
                 <div className="dropZone">
                     <h3>Drag and Drop</h3>
-                    <input type="file" className="upload"  onChange={handleFileChange}/>
+                    <input type="file" className="upload" onChange={handleFileChange} />
+                 
+                        {file && (
+                              <div className="file-details">
+                                File details:
+                                <ul>
+                                    <li>Name: {file.name}</li>
+                                    <li>Type: {file.type}</li>
+                                    <li>Size: {file.size} bytes</li>
+                                </ul>
+                                </div>
+                        )}
                 </div>
-                {file && (
-                    <section>
-                        File details:
-                        <ul>
-                            <li>Name: {file.name}</li>
-                            <li>Type: {file.type}</li>
-                            <li>Size: {file.size} bytes</li>
-                        </ul>
-                    </section>
-                )}
-
             </div>
             <div className="bu">
-                <button className="close-btn" onClick={() => props.setTrigger(false)} >Close</button>
+                <button className="close-btn" onClick={handleClose} >Close</button>
                 {props.children}
                 <button className="upload">Upload</button>
             </div>
