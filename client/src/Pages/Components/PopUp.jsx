7032cd61be2142d24c3338665a69f74209024906
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-10-15 22:52:45 -0400

    added functionality to allow for a file to be uploaded. Added space where the user can view the details of which file they uploaded.

--- client/src/Pages/Components/PopUp.jsx
+++ client/src/Pages/Components/PopUp.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import { useState } from 'react'
 import './Styles/PopUp.css'
 
-function PopUp(props){
+function PopUp(props) {
+
+
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+
+    // const handleUpload = async () => {
+    //     // We will fill this out later
+    //   };
 
     //pass in true for false value to trigger the pop up 
-    return(props.trigger) ? (
-        <div className = "popup">
-            <div className = "popup-inner">
-                <div className = "dropZone">
-                    <h3>Drag and drop</h3>
-                    <button className = "upload">Choose File From Computer</button>
+    return (props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <div className="dropZone">
+                    <h3>Drag and Drop</h3>
+                    <input type="file" className="upload"  onChange={handleFileChange}/>
                 </div>
-            </div> 
-            <button className = "close-btn" onClick={() => props.setTrigger(false)} >close</button>
+                {file && (
+                    <section>
+                        File details:
+                        <ul>
+                            <li>Name: {file.name}</li>
+                            <li>Type: {file.type}</li>
+                            <li>Size: {file.size} bytes</li>
+                        </ul>
+                    </section>
+                )}
+
+            </div>
+            <div className="bu">
+                <button className="close-btn" onClick={() => props.setTrigger(false)} >Close</button>
                 {props.children}
+                <button className="upload">Upload</button>
+            </div>
         </div>
     ) : "";
 
