b15aa9bf3977509585815c28b56d28c2e00c7d4c
Nolan Ganz <nolanganz1@gmail.com>
2024-11-01 13:09:55 -0400

    Fixed crash with fresh user with no courses

--- client/src/Pages/Components/SideMenu.jsx
+++ client/src/Pages/Components/SideMenu.jsx
@@ -63,7 +63,7 @@ function SideMenu({ courses, fetchCourses }) {
                     <h2>Welcome</h2>
                 </header>
                 <ul>
-                    {userCourses.map((course, i) => <li onClick={() => selectCourse(course)} key={i} tabIndex="0" className={userContext.selectedCourse.course_id == course.course_id ? "selected course-menu-elem" : "course-menu-elem"} > <span>{course.course_name}</span></li>)}
+                    {userContext.hasCourses ? userCourses.map((course, i) => <li onClick={() => selectCourse(course)} key={i} tabIndex="0" className={userContext.selectedCourse.course_id == course.course_id ? "selected course-menu-elem" : "course-menu-elem"} > <span>{course.course_name}</span></li>) : <></>}
                     {userContext.isInstructor ?
                         addingCourse ?
                             <>
