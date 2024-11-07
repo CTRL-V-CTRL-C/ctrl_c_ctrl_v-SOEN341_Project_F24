51a68d3769dad37724aaa66432523ca7dc29cdd7
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-11-07 17:49:49 -0500

    Checked the input of the students

--- client/cypress/e2e/RegisterTest.cy.js
+++ client/cypress/e2e/RegisterTest.cy.js
@@ -3,21 +3,34 @@ describe('All register test functions',()=> {
 
     it('Component Presence',()=> {
         cy.visit('/registerAccount') // link to the page
-        
+
         // General page 
-        cy.get('.spacer').should('be.visible') //nav bar
-        cy.get(':nth-child(2) > .navlink').should('be.visible') // login
-        cy.get(':nth-child(3) > .navlink').should('be.visible') // sign up
-        cy.get('.form').should('be.visible').should('be.visible') // form
-        cy.get('.title').should('be.visible').should('be.visible') // title
-    
-        // Student registration
+        cy.get('.spacer').should('be.visible'); //nav bar
+        cy.get(':nth-child(2) > .navlink').should('be.visible'); // login
+        cy.get(':nth-child(3) > .navlink').should('be.visible'); // sign up
+        cy.get('.form').should('be.visible'); // form
+        cy.get('.title').should('be.visible'); // title
+        cy.get('.switch > :nth-child(2)').should('be.visible'); //student tab
+        cy.get('.switch > :nth-child(3)').should('be.visible'); // instructor tab
+        cy.get(':nth-child(1) > .input').should('be.visible'); // fist name box
+        cy.get(':nth-child(2) > .input').should('be.visible'); // last name box 
+        cy.get(':nth-child(4) > .input').should('be.visible'); //email 
+        cy.get(':nth-child(5) > .input').should('be.visible'); // Id
+        cy.get(':nth-child(6) > .input').should('be.visible'); // password
+        cy.get(':nth-child(7) > .input').should('be.visible'); // password confirmed
         
-        cy.get('.switch > :nth-child(2)').should('be.visible') //student tab
-        cy.get('.switch > :nth-child(3)').should('be.visible') // instructor tab
-        cy.get(':nth-child(1) > .input')// fist name box
-        cy.get(':nth-child(2) > .input') // last name box 
-        cy.get(':nth-child(4) > .input') //email 
-        cy.get(':nth-child(4) > .input') // Id
     })
+
+  // Student registration
+        it('Verification for student input', () =>{
+            cy.visit('/registerAccount') // link to the page
+            cy.get('.switch > :nth-child(2)').click(); // clicks on the student tab
+            cy.get(':nth-child(1) > .field-label').type('test'); // enter First name
+            cy.get(':nth-child(2) > .field-label').type('testing'); // enter last name
+            cy.get(':nth-child(4) > .field-label').type('testtesting@gmail.com'); // enter email
+            cy.get(':nth-child(5) > .field-label').type('STUD0000');
+            cy.get(':nth-child(6) > .field-label').type('tester123');
+            cy.get(':nth-child(7) > .field-label').type('tester123');
+
+        })
 })
