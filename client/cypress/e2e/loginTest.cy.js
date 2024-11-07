6baea3af6f178c432c4a5028b1826b08f3eee1fc
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-11-07 18:57:51 -0500

    fixed some code, made sure to run on test data base and used correct login credentials for a test instructor and test student

--- client/cypress/e2e/loginTest.cy.js
+++ client/cypress/e2e/loginTest.cy.js
@@ -14,28 +14,31 @@ describe('All login Test functions', function(){
     it('should login instuctor with correct credentials',()=>{
         //loging in the instructor
         cy.visit('/loginAccount'); 
-        cy.get(':nth-child(2) > .field-label').type('jamesnorth@gmail.com');
+        cy.get(':nth-child(2) > .field-label').type('joeparker13@gmail.com');
         cy.get(':nth-child(3) > .field-label').type('password');
         cy.get('.submit').click();
-        //after they login need to see other elements
+        //after they login need to see intructors page (the upload button and correct switch menu)
         cy.get('.switch > :nth-child(2)').should('be.visible')// the teams page swtich bar on the left
         cy.get('.switch > :nth-child(3)').should('be.visible')//the memeberss page switch bar on the right
         cy.get('button').should('be.visible')
         cy.get(':nth-child(3) > .navlink').click();//login out
     })  
     it('should login as a student with correct credentials', () =>{
+        //logging in student 
         cy.visit('/loginAccount'); 
-        cy.get(':nth-child(2) > .field-label').type('joeparker@gmail.com');
-        cy.get(':nth-child(3) > .field-label').type('password');
+        cy.get(':nth-child(2) > .field-label').type('joeparker1@gmail.com'); //entering the email
+        cy.get(':nth-child(3) > .field-label').type('password'); //entering the password 
         cy.get('.submit').click(); //login
+        //after they login must show students view (correct switch menu)
         cy.get('button').should('be.visible')
         cy.get(':nth-child(3) > .navlink').click();//login out
     })  
     it('error message if we have wrong credentials ', ()=>{
+        //loggin in with incorrect credentials will get an error message
         cy.visit('/loginAccount'); 
-        cy.get(':nth-child(2) > .field-label').type('jamenorth@gmail.com');
-        cy.get(':nth-child(3) > .field-label').type('password1');
-        cy.get('.submit').click();
-        cy.get('.error-message > p').should('be.visible');
+        cy.get(':nth-child(2) > .field-label').type('jamenorth@gmail.com'); //entering the email
+        cy.get(':nth-child(3) > .field-label').type('password1'); //entering the password 
+        cy.get('.submit').click(); //login
+        cy.get('.error-message > p').should('be.visible'); 
     })
 })
