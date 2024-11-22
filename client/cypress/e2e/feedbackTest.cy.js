d44e289e64b4c8fb2d4388d595d74264bc0bf900
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-11-22 16:58:44 -0500

    Making sure the review is still there after refreshing page

--- client/cypress/e2e/feedbackTest.cy.js
+++ client/cypress/e2e/feedbackTest.cy.js
@@ -5,45 +5,45 @@ it('Student can successfully give feeback', ()=>{
      cy.get('#LoginPassword').type('password'); //entering the password 
      cy.get('.submit').click(); //login
 
-     //reset the form
+     //reset the form (useful here if the test fails in the middle of doing a review and we need to rerun the test)
      cy. task("deleteEvaluation", { teamId: 1, evaluatorId: 1, evaluateeId: 2 } )
 
-
-     //after user logs in makes sure they are apart of a team 
-     cy.get('.course-title').should('be.visible'); //make sure person is registered for a course
+     //Once user logs in, they can see their teams
+     cy.get('.course-title').should('be.visible').should('contain', "test_course_1"); //make sure person is registered for a course
      cy.get(':nth-child(1) > .teammate-info > :nth-child(1)').should('contain', 'joe parker').should('be.visible');//make sure name of user is the first teamate 
      cy.get(':nth-child(1) > .teammate-info > :nth-child(2)').should('contain', 'joeparker1@gmail.com').should('be.visible');//making sure users email in there
-     //user can see other members of their team and see the review button
+     //user can see other members and see the review button
      cy.get(':nth-child(2) > .teammate-info > :nth-child(1)').should('contain','joe parker').should('be.visible');
      cy.get(':nth-child(2) > .teammate-info > :nth-child(2)').should('contain', 'joeparker2@gmail.com').should('be.visible');
      cy.get(':nth-child(2) > .review-btn').should('be.visible').should('contain', 'Review').click(); //review button is there and is clicked 
+     
      //user can review the teamate 
-     cy.get('.teammate-evaluation > .team-name').should('be.visible')//heading is visible 
-     //coopeartion section with stars are visible
+     cy.get('.teammate-evaluation > .team-name').should('be.visible').should('contain','Evaluating: joe parker')//heading is visible 
+     //first criteria with 5 stars are visible
      cy.get(':nth-child(1) > .criteria-section > .criteria-name').should('contain',"COOPERATION").should('be.visible');
-     cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(1) > .sc-egkSDF > svg > path').should('be.visible')
+     cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(1) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(2) > .sc-egkSDF > svg > path').should('be.visible').click();
-     cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('be.visible').click();
+     cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('be.visible').click();//giving 3 stars 
      cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(4) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(1) > .evaluation-description').should('be.visible'); //description of criteria
-     cy.get(':nth-child(1) > .comment-box').should('be.visible').type("cooperation wasn't good"); //make sure text box is there
+     cy.get(':nth-child(1) > .comment-box').should('be.visible').type("cooperation wasn't good"); //make sure text box is there and writing a comment
      //second critera with starts
      cy.get(':nth-child(2) > .criteria-section > .criteria-name').should('contain','CONCEPTUAL CONTRIBUTION').should('be.visible');
      cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(1) > .sc-egkSDF > svg > path').should('be.visible');
-     cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(2) > .sc-egkSDF > svg > path').should('be.visible').click();//give creiteria two stars
+     cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(2) > .sc-egkSDF > svg > path').should('be.visible').click();//giving 2 stars
      cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(4) > .sc-egkSDF > svg > path').should('be.visible');
-     cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg').should('be.visible');
+     cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(2) > .evaluation-description').should('be.visible');//description of criteria
      cy.get(':nth-child(2) > .comment-box').should('be.visible').type('ok conceptual contribution');//enter text box message
      //third critera
      cy.get(':nth-child(3) > .criteria-section > .criteria-name').should('contain','PRACTICAL CONTRIBUTION').should('be.visible');
      cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(1) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(2) > .sc-egkSDF > svg > path').should('be.visible');
-     cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('be.visible').click();
+     cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('be.visible').click();//giving 3 starts
      cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(4) > .sc-egkSDF > svg > path').should('be.visible');
-     cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg').should('be.visible');
+     cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(3) > .evaluation-description').should('be.visible');
      cy.get(':nth-child(3) > .comment-box').should('be.visible').type('average practical contribution')
      //fourth criteria
@@ -51,7 +51,7 @@ it('Student can successfully give feeback', ()=>{
      cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(1) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(2) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('be.visible');
-     cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(4) > .sc-egkSDF > svg > path').should('be.visible').click();
+     cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(4) > .sc-egkSDF > svg > path').should('be.visible').click();//giving 4 stars
      cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg > path').should('be.visible');
      cy.get(':nth-child(4) > .evaluation-description').should('be.visible');
      cy.get(':nth-child(4) > .comment-box').should('be.visible').type('good work ethic');
@@ -60,7 +60,6 @@ it('Student can successfully give feeback', ()=>{
      
      //validate the form has correct information
      cy.get('h3').should('contain','Are you sure you want to submit the following evaluation ?').should('be.visible');
-    
      //first criteria 
      cy.get('.confirmation-details > :nth-child(1) > :nth-child(1)').should('contain','Criteria: COOPERATION').should('be.visible');
      cy.get('.confirmation-details > :nth-child(1) > :nth-child(2)').should("be.visible").should('contain', 'Rating: 3');//rating is visible
@@ -78,11 +77,13 @@ it('Student can successfully give feeback', ()=>{
      cy.get('.confirmation-details > :nth-child(4) > :nth-child(2)').should('be.visible').should('contain', 'Rating: 4');;
      cy.get(':nth-child(4) > :nth-child(3)').should('contain','good work ethic').should('be.visible');
 
-     //submit the form
-     cy.get('.cancel-button').should('be.visible').click(); //click cnacel button should bring us back to the review form
-     cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg > path').click()//can change one part of the review
-     cy.get(':nth-child(4) > .comment-box').type('excellent work ethic');
-     cy.get('.button__text').click();//resubmit the form
+     //before confirming the form we can go back and edit the review 
+     cy.get('.cancel-button').should('be.visible').click(); //cancel button should bring us back to the review form
+     cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg > path').click();//giving 5 stars 
+     cy.get(':nth-child(4) > .comment-box').type('excellent work ethic');//changing comment
+     cy.get('.button__text').click();//Submit the form
+     
+     //check to make sure the new edit was changed in the review 
      cy.get(':nth-child(4) > :nth-child(3)').should('contain','excellent work ethic').should('be.visible');//make sure the updates are made 
      cy.get('.confirm-button').should('be.visible').click();//confirm the form
 
@@ -91,6 +92,19 @@ it('Student can successfully give feeback', ()=>{
      cy.get('.switch > :nth-child(2)').click();//switch back to team page
      cy.get(':nth-child(2) > .review-btn').click();//click on the evaluation we just made 
 
+     //make sure the review is there with the correct information after refreshing
+     cy.get(':nth-child(1) > .comment-box').should('contain',"cooperation wasn't good");
+     cy.get(':nth-child(1) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('have.css', 'color').and('eq',"rgb(0, 0, 0)");//first criteria is 3 starts
+     cy.get(':nth-child(2) > .comment-box').should('contain', 'ok conceptual contribution');
+     cy.get(':nth-child(2) > .criteria-section > .sc-blHHSb > :nth-child(2) > .sc-egkSDF > svg > path').should('have.css', 'color').and('eq',"rgb(0, 0, 0)");//second criteria is 2 starts
+     cy.get(':nth-child(3) > .comment-box').should('contain','average practical contribution');
+     cy.get(':nth-child(3) > .criteria-section > .sc-blHHSb > :nth-child(3) > .sc-egkSDF > svg > path').should('have.css', 'color').and('eq',"rgb(0, 0, 0)");//third criteria is 3 starts
+     cy.get(':nth-child(4) > .comment-box').should('contain','excellent work ethic');
+     cy.get(':nth-child(4) > .criteria-section > .sc-blHHSb > :nth-child(5) > .sc-egkSDF > svg > path').should('have.css', 'color').and('eq',"rgb(0, 0, 0)");//fourth criteria is 5 starts
+    
+     //deleting the review
+     cy. task("deleteEvaluation", { teamId: 1, evaluatorId: 1, evaluateeId: 2 } )
+     
      //logout
      cy.get(':nth-child(3) > .navlink').click();
 
