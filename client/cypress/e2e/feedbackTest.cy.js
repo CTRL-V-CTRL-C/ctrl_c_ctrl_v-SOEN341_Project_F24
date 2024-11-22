f7f88bea6507e2736d31b1f4c823838b77426580
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-11-22 15:12:33 -0500

    Made sure that once the review is submitted the correct information is shown on the final review submission

--- client/cypress/e2e/feedbackTest.cy.js
+++ client/cypress/e2e/feedbackTest.cy.js
@@ -4,6 +4,11 @@ it('Student can successfully give feeback', ()=>{
      cy.get('#LoginEmail').type('joeparker1@gmail.com'); //entering the email
      cy.get('#LoginPassword').type('password'); //entering the password 
      cy.get('.submit').click(); //login
+
+     //reset the form
+     cy. task("deleteEvaluation", { teamId: 1, evaluatorId: 1, evaluateeId: 2 } )
+
+
      //after user logs in makes sure they are apart of a team 
      cy.get('.course-title').should('be.visible'); //make sure person is registered for a course
      cy.get(':nth-child(1) > .teammate-info > :nth-child(1)').should('contain', 'joe parker').should('be.visible');//make sure name of user is the first teamate 
@@ -56,24 +61,22 @@ it('Student can successfully give feeback', ()=>{
      //validate the form has correct information
      cy.get('h3').should('contain','Are you sure you want to submit the following evaluation ?').should('be.visible');
     
+     //first criteria 
      cy.get('.confirmation-details > :nth-child(1) > :nth-child(1)').should('contain','Criteria: COOPERATION').should('be.visible');
      cy.get('.confirmation-details > :nth-child(1) > :nth-child(2)').should("be.visible").should('contain', 'Rating: 3');//rating is visible
-     cy.get('.confirmation-details > :nth-child(1) > :nth-child(2)').should('be.visible').should('contain', "Comment: cooperation wasn't good");//comment is visible 
-    
+     cy.get('.confirmation-details > :nth-child(1) > :nth-child(3)').should('be.visible').should('contain', "Comment: cooperation wasn't good");//comment is visible 
+     //second criteria 
      cy.get('.confirmation-details > :nth-child(2) > :nth-child(1)').should('contain','Criteria: CONCEPTUAL CONTRIBUTION').should('be.visible');
-     cy.get('.confirmation-details > :nth-child(2) > :nth-child(2)').should('be.visible');
-     cy.get('.confirmation-details > :nth-child(2) > :nth-child(3)').should('be.visible');
-     //**cy.get('.confirmation-details > :nth-child(2) > :nth-child(3)').should('contain', 'ok conceptual contribution').should('be.visible');**/
-     
+     cy.get('.confirmation-details > :nth-child(2) > :nth-child(2)').should('be.visible').should('contain', 'Rating: 2');;
+     cy.get('.confirmation-details > :nth-child(2) > :nth-child(3)').should('be.visible').should('contain', 'ok conceptual contribution').should('be.visible');
+     //third criteria 
      cy.get('.confirmation-details > :nth-child(3) > :nth-child(1)').should('contain','').should('be.visible');
-     cy.get('.confirmation-details > :nth-child(3) > :nth-child(2)').should('be.visible');
-     cy.get('.confirmation-details > :nth-child(3) > :nth-child(3)').should('be.visible');
-     //**cy.get('.confirmation-details > :nth-child(3) > :nth-child(3)').should('contain','average practical contribution').should('be.visible');**/
-
+     cy.get('.confirmation-details > :nth-child(3) > :nth-child(2)').should('be.visible').should('contain', 'Rating: 3');;
+     cy.get('.confirmation-details > :nth-child(3) > :nth-child(3)').should('contain','average practical contribution').should('be.visible');
+     //fourth criteria 
      cy.get('.confirmation-details > :nth-child(4) > :nth-child(1)').should('contain','Criteria: WORK ETHIC').should('be.visible');
-     cy.get('.confirmation-details > :nth-child(4) > :nth-child(2)').should('be.visible');
-     cy.get(':nth-child(4) > :nth-child(3)').should('be.visible');
-     //**cy.get(':nth-child(4) > :nth-child(3)').should('contain','good work ethic').should('be.visible');**/
+     cy.get('.confirmation-details > :nth-child(4) > :nth-child(2)').should('be.visible').should('contain', 'Rating: 4');;
+     cy.get(':nth-child(4) > :nth-child(3)').should('contain','good work ethic').should('be.visible');
 
      //submit the form
      cy.get('.cancel-button').should('be.visible').click(); //click cnacel button should bring us back to the review form
@@ -87,16 +90,6 @@ it('Student can successfully give feeback', ()=>{
      cy.get('.switch > :nth-child(3)').click();//swtich views
      cy.get('.switch > :nth-child(2)').click();//switch back to team page
      cy.get(':nth-child(2) > .review-btn').click();//click on the evaluation we just made 
-
-     //confirm all the information is there and is correct 
-     // cy.get(':nth-child(1) > .comment-box').should('contain','cooperation wasnt good');
-     // cy.get(':nth-child(2) > .comment-box').should('contain', 'ok conceptual contribution');
-     // cy.get(':nth-child(3) > .comment-box').should('contain','average practical contribution');
-     // cy.get(':nth-child(4) > .comment-box').should('contain','excellent work ethic');
-
-     //reset the form
-     cy. task("deleteEvaluation", { teamId: 1, evaluatorId: 1, evaluateeId: 2 } )
-     
 
      //logout
      cy.get(':nth-child(3) > .navlink').click();
