4c26672196334017ea7548fdbb40ac83b0877cb0
Nolan Ganz <nolanganz1@gmail.com>
2024-11-22 17:12:53 -0500

    Added endpoint to upload images

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { router as userRouter } from './API/user.js';
 import { router as authRouter } from './API/auth.js';
 import { router as courseRouter } from './API/course.js';
+import { router as documentsRouter } from './API/document.js';
 import { router as teamRouter } from './API/team.js';
 import { router as evaluationRouter } from './API/evaluation.js';
 
@@ -21,6 +22,7 @@ apiRouter.use("/", authRouter);
 apiRouter.use("/user", userRouter);
 apiRouter.use("/team", teamRouter);
 apiRouter.use("/course", courseRouter);
+apiRouter.use("/document", documentsRouter);
 apiRouter.use("/evaluation", evaluationRouter);
 
 app.use("/api", apiRouter);
