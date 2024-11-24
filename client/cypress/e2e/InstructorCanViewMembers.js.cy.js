af050fae6ae9ae53e9a841dac2a83689075962b0
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-11-24 16:11:21 -0500

    created the array of students

--- client/cypress/e2e/InstructorCanViewMembers.js.cy.js
+++ client/cypress/e2e/InstructorCanViewMembers.js.cy.js
@@ -2,12 +2,34 @@
 describe('Instructor view of the members', function(){
 
   it('Checks the members individually', ()=>{
+  
+    //Array of students 
+    const studentsOfTheCourse = [
+      {name:'joe parker', id:'STUD1001',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1002',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1003',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1004',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1005',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1006',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1007',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1008',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1009',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1010',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1011',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1012',email:'joeparker1@gmail.com'},
+    ];
 
   // login info for the instructor 
   cy.visit('/Teams'); 
   cy.get('#LoginEmail').type('joeparker13@gmail.com');
   cy.get('#LoginPassword').type('password');
   cy.get('.submit').click();
+
+  //Navigating to the members page 
+  cy.get('#secondView').click();
+
+  //Checking the student
+
 
 
   });
