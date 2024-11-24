1492a5654d9d45520bf6897527b2cd0f7804e290
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-11-24 16:49:31 -0500

    checking the visibiliti of the components and adding some css selectors

--- client/cypress/e2e/InstructorCanViewMembers.js.cy.js
+++ client/cypress/e2e/InstructorCanViewMembers.js.cy.js
@@ -6,17 +6,17 @@ describe('Instructor view of the members', function(){
     //Array of students 
     const studentsOfTheCourse = [
       {name:'joe parker', id:'STUD1001',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1002',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1003',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1004',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1005',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1006',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1007',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1008',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1009',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1010',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1011',email:'joeparker1@gmail.com'},
-      {name:'joe parker', id:'STUD1012',email:'joeparker1@gmail.com'},
+      {name:'joe parker', id:'STUD1002',email:'joeparker2@gmail.com'},
+      {name:'joe parker', id:'STUD1003',email:'joeparker3@gmail.com'},
+      {name:'joe parker', id:'STUD1004',email:'joeparker4@gmail.com'},
+      {name:'joe parker', id:'STUD1005',email:'joeparker5@gmail.com'},
+      {name:'joe parker', id:'STUD1006',email:'joeparker6@gmail.com'},
+      {name:'joe parker', id:'STUD1007',email:'joeparker7@gmail.com'},
+      {name:'joe parker', id:'STUD1008',email:'joeparker8@gmail.com'},
+      {name:'joe parker', id:'STUD1009',email:'joeparker9@gmail.com'},
+      {name:'joe parker', id:'STUD1010',email:'joeparker10@gmail.com'},
+      {name:'joe parker', id:'STUD1011',email:'joeparker11@gmail.com'},
+      {name:'joe parker', id:'STUD1012',email:'joeparker12@gmail.com'},
     ];
 
   // login info for the instructor 
@@ -28,7 +28,11 @@ describe('Instructor view of the members', function(){
   //Navigating to the members page 
   cy.get('#secondView').click();
 
-  //Checking the student
+  //Checking the apperance 
+  cy.get('#PageTitle').should('be.visible');
+  cy.get('#studentList').should('be.visible');
+    cy.get('.studentInformation').should('be.visible');
+//Cheking if each student is present 
 
 
 
--- client/src/Pages/Components/Teams/MembersPage.jsx
+++ client/src/Pages/Components/Teams/MembersPage.jsx
@@ -37,7 +37,7 @@ function MembersPage() {
         <div id="whole">
             <div id="TitlePosition"><p id="PageTitle">Student List for {selectedCourse.course_name}</p></div>
             <div>
-                <table className="TableStyle">
+                <table id="studentList"className="TableStyle">
                     <thead>
                         <tr>
                             <th>Name</th>
@@ -47,7 +47,7 @@ function MembersPage() {
                     </thead>
                     <tbody>
                         {students.map((student, index) => (
-                            <tr key={index}>
+                            <tr class="studentInformation" key={index}>
                                 <td>{`${student.f_name} ${student.l_name}`}</td>
                                 <td>{student.school_id}</td>
                                 <td>{student.email}</td>
