a92b5cb085a8009a4123011405d02a738159f2a4
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-11-24 13:34:34 -0500

    added the login info

--- client/cypress/e2e/InstructorViewOfTheResults.cy.js
+++ client/cypress/e2e/InstructorViewOfTheResults.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
+describe('Result view of the ', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.visit('/teams');
+
+    //Login of the instructor
+    cy.get('#LoginEmail').type('joeparker13@gmail.com');
+    cy.get('#LoginPassword').type('password');
+    cy.get('.submit').click();
+
+    //accessing the results
+    cy.get(':nth-child(1) > .teammates-card > .view-results-btn').click();
+
+
+  });
+}); 
