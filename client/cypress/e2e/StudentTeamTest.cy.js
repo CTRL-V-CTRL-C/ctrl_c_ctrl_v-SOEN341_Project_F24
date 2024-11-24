2b22032a92412f6aa3cb2d78f6f2d40d3dadd9e1
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-11-24 10:49:14 -0500

    Code can finally iterate over all teammates

--- client/cypress/e2e/StudentTeamTest.cy.js
+++ client/cypress/e2e/StudentTeamTest.cy.js
@@ -1,11 +1,10 @@
 it('should display login-in user and correspoding teammates correctly', () => {
-
     const expectedTeammates = [
+        { name: 'joe parker', email: "joeparker1@gmail.com" },
         { name: 'joe parker', email: "joeparker2@gmail.com" },
         { name: 'joe parker', email: "joeparker3@gmail.com" },
         { name: 'joe parker', email: "joeparker4@gmail.com" },
     ];
-
     //logging in student 
     cy.visit('/loginAccount');
     cy.get('#LoginEmail').type('joeparker1@gmail.com'); //entering the email
@@ -16,22 +15,13 @@ it('should display login-in user and correspoding teammates correctly', () => {
     cy.get('.course-title').should('be.visible').should('contain', 'test_course_1');//couse name is visible
     cy.get('.my-team-info').should('be.visible');//team box is visible
 
-    //making sure the first elemnt is the loged-in user
-    cy.get('.teammates-card').first().within(() => {
-        cy.contains('joe parker');
-        cy.contains('joeparker1@gmail.com');
-    });
-    cy.get('.teammates-card').should('exist').and('have.length.at.least', 2);
-
     //making sure all teamate are there
-    cy.get('.teammates-card').each(($el,index) => {
-        if (index >0){
-            cy.wrap($el).within(() => { //need to wrap the team box object so that cypress can iterate over each element
-                cy.log(`Checking teammate: ${expectedTeammates[index].name}`);
-                cy.contains(expectedTeammates[index-1].name);
-                cy.contains(expectedTeammates[index-1].email);
+    cy.get('.teammates-card').each(($card) => {
+        cy.wrap($card).find('.teammate-card').each(($teammateCard,index)=>{
+            cy.wrap($teammateCard).within(() => { //need to wrap the team box object so that cypress can iterate over each element
+                cy.contains(expectedTeammates[index].name);
+                cy.contains(expectedTeammates[index].email);
             });
-        }
+        })
     });
-
 });
