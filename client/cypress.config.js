19b2908bc2fc5f93bcc35e33960c437db577d83a
Jaden Wright-Maurais <jadenwrightmaurais@gmail.com>
2024-11-23 21:40:53 -0500

    Added the fixtures path into config.js file

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     watchForFileChanges: false,
+    fixturesFolder: 'cypress/fixtures',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
