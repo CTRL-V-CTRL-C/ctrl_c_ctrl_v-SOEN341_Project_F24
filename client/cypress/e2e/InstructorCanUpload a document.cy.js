424189bf484e3596b6552be1265ac94748150681
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-11-26 18:47:02 -0500

    Finished fixing the issu

--- client/cypress/e2e/InstructorCanUpload a document.cy.js
+++ client/cypress/e2e/InstructorCanUpload a document.cy.js
@@ -17,39 +17,44 @@ describe('The instructor can upload a document', () => {
         cy.get('label.upload-button').click();
 
         // Intercept the API call for upload document 
-        cy.intercept('POST', '/api/upload/:courseId', (req) => {
+        cy.intercept('POST', '/api/document/upload/1', (req) => {
             req.reply({
                 statusCode: 200,
                 body: { message: 'The document was successfully uploaded' },
             });
-        }).as('upload/:courseId');
+        }).as('/api/document/upload/1');
+
 
         cy.get('#fileUpload').attachFile('Sprint Planning - TASKS.pdf');
         cy.get('button.upload-button').click();
 
+        cy.wait('@/api/document/upload/1').its('response.statusCode').should('eq', 200);
+
         //checking for the success message 
         cy.get('.success').should('be.visible');
 
-        // //Reuploading the same file 
+        //Reuploading the same file 
         
-        //   //clicking the upload button
-        //   cy.get('#button-upload-document').click();
-        //   //choosing a file
-        //   cy.get('label.upload-button').click();
+          //clicking the upload button
+          cy.get('#button-upload-document').click();
+          //choosing a file
+          cy.get('label.upload-button').click();
 
-        //   // Intercept the API call for upload document 
-        //   cy.intercept('POST', '/api/upload/:courseId', (req) => {
-        //       req.reply({
-        //           statusCode: 500,
-        //           body: { message: result.message },
-        //       });
-        //   }).as('upload/:courseId');
+          // Intercept the API call for upload document 
+          cy.intercept('POST', '/api/document/upload/1', (req) => {
+              req.reply({
+                  statusCode: 500,
+                  body: { message: 'Your students already have access to this document :)' },
+              });
+          }).as('/api/document/upload/1');
 
-        //   cy.get('#fileUpload').attachFile('Sprint Planning - TASKS.pdf');
-        //   cy.get('button.upload-button').click();
+          cy.get('#fileUpload').attachFile('Sprint Planning - TASKS.pdf');
+          cy.get('button.upload-button').click();
 
-        //   //checking for the error message
-        //   cy.get('#errorDocumentUpload').should('be.visible');
+          cy.wait('@/api/document/upload/1').its('response.statusCode').should('eq', 500);
+
+          //Checking for error message
+          cy.get('#errorDocumentUpload').should('be.visible');
           
           });
         });
