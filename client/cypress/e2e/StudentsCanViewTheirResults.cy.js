48166d1813603afbccf71d1630fd93992bc2057a
Carla Chamandi <157640287+Carlaeatdirt@users.noreply.github.com>
2024-11-26 23:02:43 -0500

    finished the test

--- client/cypress/e2e/StudentsCanViewTheirResults.cy.js
+++ client/cypress/e2e/StudentsCanViewTheirResults.cy.js
@@ -2,12 +2,27 @@ describe('Student can review their results', () => {
   it('passes', () => {
     cy.visit('/teams')
 
+     //login as a instructor
+     cy.get('#LoginEmail').type('joeparker13@gmail.com');
+     cy.get('#LoginPassword').type('password');
+     cy.get('.submit').click();
+
+     //releasing the reviews
+     cy.get('#release-reviews').click();
+
+    //login out
+     cy.get('#LogoutnNav').click();
+
+     // login in as a student 
     cy.get('#LoginEmail').type('joeparker1@gmail.com'); 
     cy.get('#LoginPassword').type('password'); 
     cy.get('.submit').click(); 
 
     //access reviews
     cy.get('#release-reviews').click();
+
+    //checking if their is a pop up 
+    cy.get('.eval-popup-inner').should('be.visible');
     
 
   })
